4aea1ce271bb7903a1355a5703117e703a991630
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-03-11 16:28:12 -0300

    add cookie for save data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
-import { ChallengesProvider } from "../contexts/ChallengesContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
